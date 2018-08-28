fbf34db68e11c29afd0fde3dd041f32fdaeb7759
allx <flylee.go@gmail.com>
2018-08-28 12:28:21 +0900

    remove html and css code

--- util/json2table.rb
+++ util/json2table.rb
@@ -15,52 +15,30 @@ def get_header(headers, hash, prefix = nil)
     end
 end
 
-def get_data(path, hash)
+def get_cell(path, hash)
     idx = path.index('/')
-    idx ? get_data(path[idx+1..-1], hash[path[0..idx-1]]) : hash[path]
+    idx ? get_cell(path[idx+1..-1], hash[path[0..idx-1]]) : hash[path]
 end
 
-def gen_header(headers)
-    header = ''
-    headers.each do |item|
-        header += "<th>#{item}</th>"
-    end
-    return "<thead><tr>#{header}</tr></thead>"
+def get_row(headers, hash)
+    row = []
+    headers.each { |cell| row.push(get_cell(cell, hash)) }
+    return row
 end
 
-def gen_row(headers, hash, style=nil)
-    row = ''
-    headers.each do |item|
-        data = get_data(item, hash)
-        row += "<td>#{data}</td>"
-    end
-    return "<tr #{style}>#{row}</tr>"
-end
-
-# TBD: CSS and HTML are mixed with data process
 def json_to_table(json, css = nil)
     return nil unless json
     headers = Set.new
-    rows = ''
+    data = Array.new
     if json.respond_to?(:keys)
         get_header(headers, json)
-        header= gen_header(headers)
-        rows = gen_row(headers, json)
+        data.push(get_row(headers, json))
     else
-        json.each do |item|
-            get_header(headers, item)
-        end
-        header= gen_header(headers)
-        count = 0
-        json.each do |item|
-            rows += gen_row(headers, item, count%2==1 ? "class=\"pure-table-odd\"" : nil)
-            count += 1
-        end
+        json.each { |item| get_header(headers, item) }
+        json.each { |json_row| data.push(get_row(headers, json_row)) }
     end
-    "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"pure-min.css\"></head><body><table class=\"pure-table\">#{header}<tbody>#{rows}</tbody></table></body></html>"
+    return headers, data
 end
 
-headers = Set.new
-table = JSON.parse(File.read(ARGV[0]))
-puts json_to_table(table)
-
+json = JSON.parse(File.read(ARGV[0]))
+headers, data = json_to_table(json)
